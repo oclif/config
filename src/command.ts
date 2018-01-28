39effe2d16c46c113e6cff3fcc33bad3094bfba9
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-28 15:02:47 -0800

    fix: added extra help properties

--- src/command.ts
+++ src/command.ts
@@ -1,4 +1,5 @@
 import {IConfig} from './config'
+import {IPlugin} from './plugin'
 
 export interface ICachedCommand {
   _base: string
@@ -9,15 +10,49 @@ export interface ICachedCommand {
   usage?: string
   pluginName?: string
   help?: string
+  flags: {[name: string]: ICachedFlag}
+  args: ICachedArg[]
   load(): Promise<ICommand>
+}
+
+export interface IConvertToCachedOptions {
+  id?: string
+  plugin?: IPlugin
 }
 
 export interface ICommand extends ICachedCommand {
   run(argv: string[], opts?: ICommandOptions): Promise<void>
-  convertToCached(): ICachedCommand
+  convertToCached(opts?: IConvertToCachedOptions): ICachedCommand
 }
 
 export interface ICommandOptions {
   root?: string
   config?: IConfig
 }
+
+export interface ICachedArg {
+  name: string
+  description?: string
+  required?: boolean
+  hidden?: boolean
+}
+
+export interface ICachedBooleanFlag {
+  type: 'boolean'
+  name: string
+  required?: boolean
+  char?: string
+  hidden?: boolean
+  description?: string
+}
+
+export interface ICachedOptionFlag {
+  type: 'option'
+  name: string
+  required?: boolean
+  char?: string
+  hidden?: boolean
+  description?: string
+}
+
+export type ICachedFlag = ICachedBooleanFlag | ICachedOptionFlag
