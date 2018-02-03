cec67ea6805e373d4f9241fffb0036a9a6fea118
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-02-03 06:45:53 -0800

    fix: move up config require just in case

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import 'fs-extra-debug'
 
+export {IConfig, Config, Options, load} from './config'
 export {Command} from './command'
 export {Hook, Hooks} from './hooks'
 export {Manifest} from './manifest'
 export {PJSON} from './pjson'
 export {IPlugin, Plugin} from './plugin'
-export {IConfig, Config, Options, load} from './config'
 export {Topic} from './topic'
