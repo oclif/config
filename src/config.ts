2d0d2c330e5f2d37ca99e6a702ca9fef6a4b837b
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-06 21:14:51 -0700

    fix: added channel

--- src/config.ts
+++ src/config.ts
@@ -24,6 +24,7 @@ const debug = Debug()
 export interface IConfig {
   name: string
   version: string
+  channel?: string
   pjson: PJSON.CLI
   /**
    * process.arch
@@ -118,6 +119,7 @@ export class Config implements IConfig {
   _base = `${_pjson.name}@${_pjson.version}`
   name!: string
   version!: string
+  channel?: string
   root!: string
   arch!: ArchTypes
   bin!: string
@@ -147,6 +149,7 @@ export class Config implements IConfig {
     this.pjson = plugin.pjson
     this.name = this.pjson.name
     this.version = this.pjson.version
+    this.channel = this.pjson.channel
 
     this.arch = (os.arch() === 'ia32' ? 'x86' : os.arch() as any)
     this.platform = os.platform() as any
